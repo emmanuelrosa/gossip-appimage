10198a638babbce42ebead92f9cc33e4084770b3
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2024-01-07 22:08:57 -0500

    add gossip-ld-library-path

--- flake.nix
+++ flake.nix
@@ -65,7 +65,48 @@
         '';
       };
 
-      gossip-appimage = nix-appimage.bundlers.x86_64-linux.default self.packages.x86_64-linux.gossip-patchelf;
+      gossip-ld-library-path = let
+        gossip = erosanix.packages.x86_64-linux.gossip;
+
+        launcher = pkgs.writeScript "gossip-launcher" ''
+          #!${pkgs.bash}/bin/bash
+          LD_LIBRARY_PATH=${libraryPath}/lib:/usr/lib/${system}-gnu:/usr/lib:/lib64:/usr/lib64 ${gossip}/bin/gossip
+        '';
+
+        libraryPath = pkgs.symlinkJoin { 
+          name = "gossip-library-path";
+          paths = builtins.map 
+            (p: pkgs.lib.attrsets.getLib p) 
+            (pkgs.lib.strings.splitString "\n" 
+              (builtins.readFile (pkgs.writeReferencesToFile gossip)));
+        };
+      in pkgs.stdenv.mkDerivation {
+        pname = "gossip-ld-library-path";
+        version = gossip.version;
+        src = gossip;
+        dontUnpack = true;
+        meta = gossip.meta;
+        dontPatchELF = true;
+
+        installPhase = ''
+          runHook preInstall
+          mkdir -p $out/bin
+
+          cp ${launcher} $out/bin/gossip
+
+          if [[ -h $src/share ]]
+          then
+            ln -s $(readlink $src/share) $out/share
+          elif [[ -d $src/share ]]
+          then
+            ln -s $src/share $out/share
+          fi
+
+          runHook postInstall
+        '';
+      };
+
+      gossip-appimage = nix-appimage.bundlers.x86_64-linux.default self.packages.x86_64-linux.gossip-ld-library-path;
     };
   };
 }
